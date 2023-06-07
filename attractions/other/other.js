0363cb48b13b11b97a7cd261ca2c3f03f49c3fc1
brocklarson <brock.larson0725@gmail.com>
2023-06-07 08:33:21 -0600

    Finish updating attraction pages

--- attractions/other/other.js
+++ attractions/other/other.js
@@ -2,30 +2,35 @@ import { createUI } from "../attraction-details.js";
 import { other } from "../attractions-class.js";
 
 
-let title = "Spring City"
-let description = `Spring City is one of the prettiest small towns in the country. And it's not just us that think so! Forbes listed it as one of the top prettiest towns in America! In addition, only this town and Williamsburg, VA are listed as complete towns on the national other registry, making this a place you must visit!`;
-let img = {src: "/assetts/images/Activities/015.jpg", alt: "Antique cabin in historical spring city"}
+let title = "Palisade State Park"
+let description = `Palisades has a little something for everyone! Whether it be paddleboards and canoes, grilling dinner while swimming in the lake, fishing, hiking, or golfing, Palisade State Park is a place where you can easily spend the entire day! There are canoes and paddleboards for rent here!`;
+let img = {src: "/assetts/images/Activities/palisades.jpg", alt: "Canoes and paddle boats on the lake at palisade state park"}
 let shortInfo = ``;
-let attractionLink = `http://sanpete.com/pages/historicalspringcity/`;
+let attractionLink = `https://stateparks.utah.gov/parks/palisade/`;
 other.addAttraction(title, description, shortInfo, img, attractionLink);
 
-title = "Columbian Mammoth"
-description = `That's right! Fairview Museum of other and Art houses a nearly fully intact Columbian mammoth that was unearthed only miles away in 1988. The quality and altitude at which it was found make this Mammoth remarkably well-preserved!`;
-img = {src: "/assetts/images/Activities/003.jpg", alt: "Historic columbian mammoth at museum in Sanpete, Utah"}
-attractionLink = `http://sanpete.com/businesses/show/313-fairview-museum-of-other-and-art`;
+title = "Gunnison Reservoir"
+description = `Gunnison Reservoir is a bit smaller than nearby Yuba State Park, but we recommend it since it's a free lake for everyone to use to go boating or waterskiing that is also easily accessible and only a short drive from the cabins! There are boat ramps for the public to use and beaches to set up camp!`;
+img = {src: "/assetts/images/Activities/gunnison-res.jpg", alt: "Birds-eye view of the peaceful gunnison reservoir"}
+attractionLink = `https://www.bivy.com/adventures/us/utah/gunnison-reservoir-117007`;
 other.addAttraction(title, description, shortInfo, img, attractionLink);
 
-title = "Ephraim/Manti"
-description = `Our little Ephraim and Manti city are home to several historical sites from the impressive Manti Temple built in the 1880s to the Chief Blackhawk Massacre monument. Between Ephraim and neighboring Manti city, there's enough to keep both other and non-other buffs entertained all day!`;
-img = {src: "/assetts/images/Activities/017.jpeg", alt: "Historic home in Ephraim, Utah"}
-attractionLink = `https://www.ephraimcity.org/DocumentCenter/View/152/Walking-Tour-of-Ephraim-PDF?bidId=`;
+title = "Fishing"
+description = `Sanpete has an endless supply of fantastic waterholes for you to enjoy some fishing while also enjoying some of the beautiful landscape around you! One of our favorite spots is Lake Hill up Ephraim Canyon, but there are countless locations for you to go fishing all over Sanpete County!`;
+img = {src: "/assetts/images/Activities/fishing.jpg", alt: "Two kids fishing in Ephraim canyon"}
+attractionLink = `http://sanpete.com/pages/fish`;
 other.addAttraction(title, description, shortInfo, img, attractionLink);
 
-title = "Ed Roth Museum"
-description = `Ed "Big Daddy" Roth was a genius and a visionary. He was responsible for so many "firsts," and literally changed the world of hot rodding. As an artist, cartoonist, illustrator, pinstriper, and custom car designer, he is most well-known for the hot rod icon "Rat Fink".`;
-img = {src: "/assetts/images/Activities/013.jpg", alt: "Custom hot rod car at famous Ed Roth Museum"}
-attractionLink = `http://www.ratfink.com/ed-roth-museum.php`;
+title = "Basin Drive-In Theater"
+description = `Nothing ends the day quite like sitting under the stars around a firepit. And the next best thing after that is sitting in an old-fashioned drive-in movie theater! So after you finish off the day, come and relax and watch the latest movie on the big screen! Movies start at dusk.`;
+img = {src: "/assetts/images/Activities/012.jpg", alt: "Historice basin drive-int theater sign"}
+attractionLink = `https://www.driveinmovie.com/ut/mount-pleasant/basin-drive-in/`;
+other.addAttraction(title, description, shortInfo, img, attractionLink);
+
+title = "Manti Aquatic Center"
+description = `The pool in Manti city is a great place for the whole family to have some fun playing in the water! This aquatic center has water slides, water games and toys, and more! So if you're looking for a great afternoon at the pool then look no further than here!`;
+img = {src: "/assetts/images/Activities/manti-pool.jpg", alt: "Families playing at the manti aquatic center pool"}
+attractionLink = `https://www.manticity.com/swimmingpool`;
 other.addAttraction(title, description, shortInfo, img, attractionLink);
 
 createUI(other);
-
--- attractions/sightseeing/sightseeing.js
+++ attractions/sightseeing/sightseeing.js
@@ -2,29 +2,29 @@ import { createUI } from "../attraction-details.js";
 import { sightseeing } from "../attractions-class.js";
 
 
-let title = "Spring City"
-let description = `Spring City is one of the prettiest small towns in the country. And it's not just us that think so! Forbes listed it as one of the top prettiest towns in America! In addition, only this town and Williamsburg, VA are listed as complete towns on the national sightseeing registry, making this a place you must visit!`;
-let img = {src: "/assetts/images/Activities/015.jpg", alt: "Antique cabin in historical spring city"}
+let title = "Skyline Drive"
+let description = `If you want to see the beauty that Sanpete has to offer, then look no further than the drive up to and along Skyline drive! Skyline drive stretches on for nearly a hundred miles and at over 10,000 feet it ranks among the highest roads in America! Get your cameras ready for the breathtaking views!`;
+let img = {src: "/assetts/images/Activities/026.jpg", alt: "View from the top of skyline drive at sunrise in Sanpete, Utah"}
 let shortInfo = ``;
-let attractionLink = `http://sanpete.com/pages/historicalspringcity/`;
+let attractionLink = `https://www.go-utah.com/Skyline-Drive/`;
 sightseeing.addAttraction(title, description, shortInfo, img, attractionLink);
 
-title = "Columbian Mammoth"
-description = `That's right! Fairview Museum of sightseeing and Art houses a nearly fully intact Columbian mammoth that was unearthed only miles away in 1988. The quality and altitude at which it was found make this Mammoth remarkably well-preserved!`;
-img = {src: "/assetts/images/Activities/003.jpg", alt: "Historic columbian mammoth at museum in Sanpete, Utah"}
-attractionLink = `http://sanpete.com/businesses/show/313-fairview-museum-of-sightseeing-and-art`;
+title = "Nebo Loop National Scenic Byway"
+description = `From Nephi to Payson, this 37 mile route has breathtaking views of the Wasatch Range, passing by Mt. Nebo which reaches 11,929 feet! Be prepared to see flat bottomlands, high-alpine conifers, red rock formations, gray sandstone cliffs, and salt flats all in the same day!`;
+img = {src: "/assetts/images/Activities/nebo-scenic-loop.jpg", alt: "View of red rocks on mount nebo scenic loop in Sanpete, Utah"}
+attractionLink = `https://www.go-utah.com/Nebo-Loop-Scenic-Byway/`;
 sightseeing.addAttraction(title, description, shortInfo, img, attractionLink);
 
-title = "Ephraim/Manti"
-description = `Our little Ephraim and Manti city are home to several historical sites from the impressive Manti Temple built in the 1880s to the Chief Blackhawk Massacre monument. Between Ephraim and neighboring Manti city, there's enough to keep both sightseeing and non-sightseeing buffs entertained all day!`;
-img = {src: "/assetts/images/Activities/017.jpeg", alt: "Historic home in Ephraim, Utah"}
-attractionLink = `https://www.ephraimcity.org/DocumentCenter/View/152/Walking-Tour-of-Ephraim-PDF?bidId=`;
+title = "Manti Temple"
+description = `Built from 1875 to 1893, the Manti temple is an impressive and beautiful building built on top of "temple hill" in Manti, Utah. The temple is a special place of worship for members of The Church of Jesus Christ of Latter-Day Saints, but its beauty is appreciated by all!`;
+img = {src: "/assetts/images/Activities/manti-temple.jpg", alt: "Historic manti temple"}
+attractionLink = `http://sanpete.com/pages/temple`;
 sightseeing.addAttraction(title, description, shortInfo, img, attractionLink);
 
-title = "Ed Roth Museum"
-description = `Ed "Big Daddy" Roth was a genius and a visionary. He was responsible for so many "firsts," and literally changed the world of hot rodding. As an artist, cartoonist, illustrator, pinstriper, and custom car designer, he is most well-known for the hot rod icon "Rat Fink".`;
-img = {src: "/assetts/images/Activities/013.jpg", alt: "Custom hot rod car at famous Ed Roth Museum"}
-attractionLink = `http://www.ratfink.com/ed-roth-museum.php`;
+title = "Cascading Waterfalls"
+description = `These cascading waterfalls are a hidden gem that you won't read about anywhere else! Make sure you walk up and down the river because there is a half mile of beautiful cascading waterfalls! Make sure to read the detailed instructions for the cascading waterfalls on our map page!`;
+img = {src: "/assetts/images/Activities/022.jpg", alt: "Hidden cascading waterfalls up ephraim canyon in Sanpete, Utah"}
+attractionLink = `https://www.google.com/maps/d/viewer?hl=en&mid=1GllTUOl9d9QD_GAVsYzoRNSFgxHPmlfb&ll=39.262675491612576%2C-111.41255300880948&z=15`;
 sightseeing.addAttraction(title, description, shortInfo, img, attractionLink);
 
 createUI(sightseeing);
