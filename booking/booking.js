5f8658e244f0aacb2505f4da7357101c3fd847da
brocklarson <brock.larson0725@gmail.com>
2023-06-23 17:49:46 -0600

    Add booking confirmation text

--- booking/booking.js
+++ booking/booking.js
@@ -1,4 +1,6 @@
-buttonsModule = (() => {
+import { mainCabin, guestCabin, bothCabins } from "/cabins/cabin-class.js";
+
+const buttonsModule = (() => {
     let STEP = 1;
     const nextButton = document.querySelector('#booking-next');
     const buttons = Array.from(document.querySelectorAll(".booking-steps-button"));
@@ -48,8 +50,6 @@ buttonsModule = (() => {
 
         return invalid;
     }
-    function validateStep3(){}
-    function validateStep4(){}
 
     function validateForm(event){
         let invalid = false;
@@ -60,8 +60,6 @@ buttonsModule = (() => {
 
         if(STEP === 1) invalid = validateStep1();
         if(STEP === 2) invalid = validateStep2();
-        if(STEP === 3) invalid = validateStep3();
-        if(STEP === 4) invalid = validateStep4();
 
         if(invalid === true) return;
         determineStep(event);
@@ -118,8 +116,8 @@ buttonsModule = (() => {
             completedStep[i - 1].classList.remove('hide');
         }
         if(STEP > 1) stepOneDone();
-        if(STEP > 2) stepTwoDone();        
-
+        if(STEP > 2) stepTwoDone(); 
+        if(STEP > 3) confirmBooking();       
     }
 
     function stepOneDone(){
@@ -141,12 +139,112 @@ buttonsModule = (() => {
     }
 
     function stepTwoDone(){
-        const guestName = document.getElementById('first-name').value  + " " + document.getElementById('last-name').value; //puts first name and last name together
-        document.getElementById("step-two-done-name").innerText = guestName; //puts name in step two done section
+        const guestName = document.getElementById('first-name').value  + " " + document.getElementById('last-name').value; 
+        document.getElementById("step-two-done-name").innerText = guestName;
         const guestEmail = document.getElementById("guest-email").value;
-        document.getElementById("step-two-done-email").innerText = guestEmail; //Puts email in the step two done section
+        document.getElementById("step-two-done-email").innerText = guestEmail; 
         const phoneNumber = formatPhoneNumber(document.getElementById("guest-phone").value);
         document.getElementById("step-two-done-phone").innerText = phoneNumber;
+    }
+
+    function getDates(){
+        //Gets the dates, parses the date, makes it look in the format of "Jan 01, 2019" (like above)
+        const startDate = document.getElementById('date-select').value.slice(0,10); 
+        const endDate = document.getElementById('date-select').value.slice(13,23); 
+        const startDateFormatted = new Date(Date.parse(startDate));
+        const endDateFormatted = new Date(Date.parse(endDate));
+        const startDateParse = startDateFormatted.toString().slice(0,15);
+        const endDateParse = endDateFormatted.toString().slice(0,15);
+        const totalNights = Math.round((endDateFormatted-startDateFormatted)/(1000*60*60*24));
+        const confirmStartDate = startDateParse.slice(0,3) + ", " + startDateParse.slice(8,11) + startDateParse.slice(4,8) + startDateParse.slice(11,15); 
+        const confirmEndDate = endDateParse.slice(0,3) + ", " + endDateParse.slice(8,11) + endDateParse.slice(4,8) + endDateParse.slice(11,15);
+
+        return [confirmStartDate, confirmEndDate, totalNights]
+    }
+
+    const calcNightlyCost = (cabinSelection, nights, guests, pets) => {
+        let cabinCost;
+        let cabin;
+
+        if(cabinSelection === "Main Cabin") cabin = mainCabin;
+        else if(cabinSelection === "Guest Cabin") cabin = guestCabin;
+        else cabin = bothCabins;
+
+        if(nights >= 7) cabinCost = cabin.week_price / 7 * nights;
+        else cabinCost = cabin.night_price * nights;
+
+        let guestCharge = 0;
+        let petCharge = 0;
+        if(guests > 10){
+            guestCharge = 5 * (guests - 10);
+            cabinCost += guestCharge;
+        }
+        if(pets >= 1){
+            petCharge = 20 * pets;
+            cabinCost += petCharge;
+        }
+        const totalCost = cabinCost.toFixed(2).toString();
+        document.getElementById("confirm-cost").innerText = totalCost;
+        document.getElementById("confirm-cost0").value = totalCost;
+
+        // document.getElementById("cost-calculator").innerHTML = cabinSelection + ": $" + cabinCost.toFixed(2) +"<br>Extra Guests: $" + guestCharge + "<br>Pet Charge: $" + petCharge;
+        // document.getElementById("cost-calculator0").value = cabinSelection + ": $" + cabinCost.toFixed(2) +"<br>Extra Guests: $" + guestCharge + "<br>Pet Charge: $" + petCharge;
+      }
+
+    //Function for displaying all the information neatly (The variables ending in "0" are not displayed but are the variables sent to the email in the <form> section of bookNow.html)
+    function confirmBooking(){         
+        const firstName = document.getElementById('first-name').value;
+        const lastName = document.getElementById('last-name').value;
+        const email = document.getElementById('guest-email').value;
+        const phone = formatPhoneNumber(document.getElementById('guest-phone').value);
+        const totalGuests = Number(document.getElementById('adults').value) + Number(document.getElementById('children').value);
+        const totalAdults = document.getElementById('adults').value;
+        const totalChildren = document.getElementById('children').value;
+        const totalPets = Number(document.getElementById('pets').value);
+        const cabinSelection = document.getElementById('cabin-selection');
+        const cabin = cabinSelection.options[cabinSelection.selectedIndex].text;
+        const [startDate, EndDate, totalNights] = getDates();
+        const comments = document.getElementById('add-comment').value;
+
+        //First and last names
+        document.getElementById("confirm-name").innerText = firstName  + " " + lastName;
+        document.getElementById("confirm-name0").value = firstName  + " " + lastName;
+        //Email
+        document.getElementById("confirm-email").innerText = email;
+        document.getElementById("confirm-email0").value = email;
+        //Phone number
+        document.getElementById("confirm-phone").innerText = phone;
+        document.getElementById("confirm-phone0").value = phone;
+        //Number of guests
+        document.getElementById("confirm-totalNumber").innerText = totalGuests;
+        document.getElementById("confirm-totalNumber0").value = totalGuests;
+        //Number of adultes
+        document.getElementById("confirm-adults").innerText = totalAdults;
+        document.getElementById("confirm-adults0").value = totalAdults;
+        //Number of children
+        document.getElementById("confirm-children").innerText = totalChildren;
+        document.getElementById("confirm-children0").value = totalChildren;
+        //Number of pets
+        
+        document.getElementById("confirm-pets").innerText = totalPets;
+        document.getElementById("confirm-pets0").value = totalPets;
+        //Cabin selection
+        document.getElementById("confirm-cabin").innerText = cabin;
+        document.getElementById("confirm-cabin0").value = cabin;
+        //Gets the dates, parses the date, makes it look in the format of "Jan 01, 2019"
+        document.getElementById("confirm-startDate").innerText = startDate;
+        document.getElementById("confirm-startDate0").value = startDate;
+        document.getElementById("confirm-endDate").innerText = EndDate;
+        document.getElementById("confirm-endDate0").value = EndDate;
+        document.getElementById("confirm-daterange0").value = document.getElementById("date-select").value;
+        //Number of nights
+        document.getElementById("confirm-nights").innerText = totalNights;
+        document.getElementById("confirm-nights0").value = totalNights;
+        //Adds their person coments
+        document.getElementById("confirm-message").innerText = comments;
+        document.getElementById("confirm-message0").value = comments;
+
+        calcNightlyCost(cabin, totalNights, totalGuests, totalPets);
     }
 
     (function init(){
